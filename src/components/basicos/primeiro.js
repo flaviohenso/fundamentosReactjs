ac657f124704645933726325a85756e8624e27ce
flaviohenso <flavio.henso@gmail.com>
2022-02-13 13:42:01 -0300

    Primeiro componente #02

--- src/components/basicos/primeiro.js
+++ src/components/basicos/primeiro.js
@@ -1,4 +1,12 @@
 
+const nome = "Seja bem vindo!"
+const retorno = 
+    <div>
+        <h1>Meu primeiro componente</h1>
+        <h2>{nome}</h2>
+    </div> 
+
+
 export default function primeiro(){
-    return 'primeiro componente'
+    return  retorno
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import './index.css'
 import ReactDOM from 'react-dom'
 import Primeiro from './components/basicos/primeiro'
 
-const content = <strong>Olá React!!</strong>
 
 ReactDOM.render(
     <div>
-        {   content }
         <Primeiro></Primeiro>
     </div>,
     document.getElementById('root')
