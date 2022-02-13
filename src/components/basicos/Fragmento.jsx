b781d441103d34e9c2ff650c7e7bf361db490876
flaviohenso <flavio.henso@gmail.com>
2022-02-13 14:24:46 -0300

    React Fragment

---
+++ src/components/basicos/Fragmento.jsx
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+
+export default function Fragmento(){
+    return(
+        <React.Fragment>
+            <h1>Teste de fragmento</h1>
+            <p>Inserindo a tag de fragmento do react</p>
+        </React.Fragment>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import './index.css'
 import ReactDOM from 'react-dom'
 import Primeiro from './components/basicos/primeiro'
 import ComParametro from './components/basicos/comParametro'
+import Fragmento from './components/basicos/Fragmento'
 
 
 ReactDOM.render(
-    <div>
+    <>
         <Primeiro></Primeiro>
         <ComParametro nome="Flavio Henrique" disciplina="Bando de dados" nota={9.9}/>
         <ComParametro nome="Clara Sophie" disciplina="Bando de dados" nota={9.9}/>
-    </div>,
+        <Fragmento/>
+    </>,
     document.getElementById('root')
     )
