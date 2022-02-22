b4f13eee4c437291e8a7f89d632383c3f82218cd
flaviohenso <flavio.henso@gmail.com>
2022-02-22 05:13:07 -0300

    Componente Card #02

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import './App.css'
 import Primeiro from './components/basicos/primeiro'
 import ComParametro from './components/basicos/comParametro'
 import Fragmento from './components/basicos/Fragmento'
@@ -5,21 +6,22 @@ import DesafioNumAleatorio from './components/basicos/DesafioNumAleatorio'
 import Card from './components/layout/Card'
 
 export default _ =>
-    <>
-
-        <Card titulo="Primeiro Componente" conteudo="Conteudo do card">
-            <Primeiro></Primeiro>
-        </Card>
-        <Card titulo="Componente com parametro" conteudo="Conteudo do card">
-            <ComParametro nome="Flavio Henrique" disciplina="Bando de dados" nota={9.9} />
-            <ComParametro nome="Clara Sophie" disciplina="Bando de dados" nota={9.9} />
-        </Card>
-        <Card titulo="Fragemento" conteudo="Conteudo do card">
-            <Fragmento />
-        </Card>
-        <Card titulo="Desafil aleatorio" conteudo="Conteudo do card">
-            <DesafioNumAleatorio min={1} max={10} />
-        </Card>
-        <Card titulo="Card1" conteudo="Conteudo do card" />
-    </>
-
+    <div className='App'>
+        <h1>Fundamentos React</h1>
+        <div className='cards'>
+            <Card titulo="Primeiro Componente" conteudo="Conteudo do card">
+                <Primeiro></Primeiro>
+            </Card>
+            <Card titulo="Componente com parametro" conteudo="Conteudo do card">
+                <ComParametro nome="Flavio Henrique" disciplina="Bando de dados" nota={9.9} />
+                <ComParametro nome="Clara Sophie" disciplina="Bando de dados" nota={9.9} />
+            </Card>
+            <Card titulo="Fragemento" conteudo="Conteudo do card">
+                <Fragmento />
+            </Card>
+            <Card titulo="Desafil aleatorio" conteudo="Conteudo do card">
+                <DesafioNumAleatorio min={1} max={10} />
+            </Card>
+            <Card titulo="Card1" conteudo="Conteudo do card" />
+        </div>
+    </div>
